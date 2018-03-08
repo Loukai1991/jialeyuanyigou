7c30a771ee45c165e707b5a6cc3f1dee0d71a0c0
liuweime <liuweiprog@foxmail.com>
2018-03-08 14:39:00 +0800

    deleted:    frontend/pages/rituall/rituall.wxml

---
+++ php/App/Api/Controller/UploadImgController.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 文件名：UploadImg.php
+ *
+ * 模块：
+ *
+ * 功能：
+ *
+ * @copyright Copyright (c) 2015 – www.hongshu.com
+ * @author    : Administrator
+ * @version   :
+ */
+
+namespace Api\Controller;
+
+class UploadImg extends PublicController
+{
+    public function uploadimg(){
+   
+        //调用上传类
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     100 * 1024 ;// 设置附件上传大小
+        $upload->exts      =     array('jpg', 'gif', 'png');// 设置附件上传类型
+        $upload->rootPath  =   '/var/www/html/Data/UploadFiles/fromminiprogram/'; // 设置附件上传根目录
+//        $upload->savePath  =  dirname(getBookStaticFilepath($bid)).'/'; // 设置附件上传（子）目录
+        $upload->autoSub = false;  //不要生成日期子目录
+    
+    
+        //上传文件
+        $info   =   $upload->upload();
+        
+        if(!$info) {    // 上传错误提示错误信息
+            $result['code'] = 0;
+            $result['msg'] = $upload->getError();
+            $this->ajaxReturn($result);
+        }
+        echo json_encode(array('status'=>1,'msg'=>'上传成功','data'=>$info));
+        exit();
+    
+    }
+}
